012a12cd49f441c6bf97312d4ccde006b01ed38f
Cauã Araujo <caua.vitor.araujo13@gmail.com>
2026-09-11 13:57:38 -0300

    Iniciando a classe "Matriz"

--- src/models/matriz.py
+++ src/models/matriz.py
@@ -1,3 +1,9 @@
 class Matriz:
-    def __init__() -> None:
-        pass
+    def __init__(self, linhas, colunas):
+        self.linhas = linhas
+        self.colunas = colunas
+        self.dados = []
+        self.pontos_entrega = {}
+        self.origem = None
+
+o
