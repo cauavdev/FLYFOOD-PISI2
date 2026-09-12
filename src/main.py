d0c900d4b2b0551fb0e207ce297a1192cd10e4c3
Cauã Araujo <caua.vitor.araujo13@gmail.com>
2026-09-12 11:13:15 -0300

    Implementação da Main e arquivo de entrada de exemplo

--- src/main.py
+++ src/main.py
@@ -1,3 +1,26 @@
+import sys
+from models.matriz import Matriz
+
+def main():
+    print("=" * 20)
+    print("FlyFood - PISI-2")
+    print("=" * 20)
+    
+    matriz = Matriz(0, 0)
+    
+    caminho_entrada = "input/matriz_entrada.txt"
+    print(f"\ncarregando arquivo: {caminho_entrada}")
+    
+    if not matriz.carregar_do_arquivo(caminho_entrada):
+        print("falha ao carregar arquivo!")
+        return
+    
+    print("arquivo carregado!")
+    
+    print("\nMatriz Carregada")
+    matriz.exibir()
 
 
 
+if __name__ == "__main__":
+    main()
