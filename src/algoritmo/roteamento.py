a8edc48695db808fa0c89bd0db184afb7eb5fd4e
Cauã Araujo <caua.vitor.araujo13@gmail.com>
2026-09-14 20:02:06 -0300

    Algoritmo de Roteamento

---
+++ src/algoritmo/roteamento.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+from utils.distancia import calcular_distancia_rota, formatar_rota
+
+
+def calcular_melhor_rota(matriz):
+    pontos = list(matriz.obter_pontos_entrega().keys())
+
+    if not pontos:
+        return [], 0
+
+    melhor_rota = None
+    melhor_distancia = float('inf')
+
+    for permutacao in permutations(pontos):
+        rota_candidata = list(permutacao)
+        distancia = calcular_distancia_rota(matriz, rota_candidata)
+
+        if distancia < melhor_distancia:
+            melhor_distancia = distancia
+            melhor_rota = rota_candidata
+
+    return melhor_rota, melhor_distancia
+
+
+if _name_ == '_main_':
+    import sys
+    import os
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+    from models.matriz import Matriz
+
+    matriz = Matriz(0, 0)
+    matriz.carregar_de_arquivo('input/matriz_entrada.txt')
+
+    rota_otima, dist_otima = calcular_melhor_rota(matriz)
+    print(f"Resposta: \"{formatar_rota(rota_otima)}\"")
+    print(f"Distância: {dist_otima} dronômetros")
